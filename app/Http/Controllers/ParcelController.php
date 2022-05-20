d7e211c77f2685cd3b8cf683e93cca9e0cd33641
ling xiao <lingxiao3218@gmail.com>
2022-05-20 12:58:38 +0800

    feat: add parcel complete

--- app/Http/Controllers/ParcelController.php
+++ app/Http/Controllers/ParcelController.php
@@ -2,11 +2,42 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Parcel;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ParcelController extends Controller
 {
-    public function sendParcel(Request $request) {
-        dd($request);
+    public function sendParcel(Request $request)
+    {
+        $request->validate([
+            "sender_postcode" => "required",
+            "recipient_firstname" => "required",
+            "recipient_lastname" => "required",
+            "recipient_address" => "required",
+            "recipient_postcode" => "required",
+            "recipient_phone" => "required",
+            "weight" => "required",
+        ]);
+
+        $currentLoggedInId = Auth::user()->id;
+
+        $parcel = new Parcel;
+        $parcel->tracking_number = 0;
+        $parcel->weight = $request->weight;
+        $parcel->sender_id = $currentLoggedInId;
+        $parcel->sender_address = $request->sender_address;
+        $parcel->sender_postcode = $request->sender_postcode;
+        $parcel->recipient_firstname = $request->recipient_firstname;
+        $parcel->recipient_lastname = $request->recipient_lastname;
+        $parcel->recipient_address = $request->recipient_address;
+        $parcel->recipient_postcode = $request->recipient_postcode;
+        $parcel->recipient_phone = $request->recipient_phone;
+        $parcel->status = 0;
+
+        $parcel->save();
+
+        // redirect to homepage
+        return redirect()->route('root');
     }
 }
--- resources/views/senderHomePage.blade.php
+++ resources/views/senderHomePage.blade.php
@@ -3,117 +3,127 @@
 <html>
 
 <head>
-  <meta charset="utf-8">
-  <title>Sender homepage</title>
-  <link rel="stylesheet" href="style.css">
+    <meta charset="utf-8">
+    <title>Sender homepage</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body class="center">
-  <header>
-    <h2>Sender Homepage</h2>
+    <header>
+        <h2>Sender Homepage</h2>
 
-    <nav>
-      <li><a href="#">Home</a></li>
-      <li><a href="#">nav 2</a></li>
-      <li><a href="#">nav 3</a></li>
-      <li><a href="#">nav 4</a></li>
-    </nav>
-  </header>
+        <nav>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">nav 2</a></li>
+            <li><a href="#">nav 3</a></li>
+            <li><a href="#">nav 4</a></li>
+        </nav>
+    </header>
 
-  <main>
-    <h1 class="page-title">Parcel Info</h1>
-    <form class="center" action="{{ route('parcel.send') }}" method="POST">
-      @csrf
-      <div>
-        <label for="address">Sender address:</label>
-        <input type="text" name="sender-address" id="address" required>
-      </div>
-      <div>
-        <label for="address">Sender postcode:</label>
-        <input type="number" name="sender-postcode" id="address" required>
-      </div>
-      <div>
-        <label for="recepient">Receipient first name:</label>
-        <input type="text" name="recepient-first-name" id="recepient-name" required>
-      </div>
-      <div>
-        <label for="recepient">Receipient last name:</label>
-        <input type="text" name="recepient-last-name" id="recepient-name" required>
-      </div>
-      <div>
-        <label for="address">Receipient address:</label>
-        <input type="text" name="receipient-address" id="address" required>
-      </div>
-      <div>
-        <label for="address">Receipient postcode:</label>
-        <input type="number" name="receipient-postcode" id="address" required>
-      </div>
-      <div>
-        <label for="address">Receipient phone:</label>
-        <input type="number" name="receipient-postcode" id="address" required>
-      </div>
-      <div>
-        <label for="weight">Package weight:</label>
-        <input type="number" name="weight" id="weight" required>
-      </div>
+    <main>
+        @if ($errors->any())
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        @endif
+        <h1 class="page-title">Parcel Info</h1>
+        <form class="center" action="{{ route('parcel.send') }}" method="POST">
+            @csrf
+            <div>
+                <label for="address">Sender address:</label>
+                <input type="text" name="sender_address" id="address" required>
+            </div>
+            <div>
+                <label for="address">Sender postcode:</label>
+                <input type="number" name="sender_postcode" id="address" required>
+            </div>
+            <div>
+                <label for="recepient">recipient first name:</label>
+                <input type="text" name="recipient_firstname" id="recepient-name" required>
+            </div>
+            <div>
+                <label for="recepient">recipient last name:</label>
+                <input type="text" name="recipient_lastname" id="recepient-name" required>
+            </div>
+            <div>
+                <label for="address">recipient address:</label>
+                <input type="text" name="recipient_address" id="address" autocomplete="address-line1" required>
+            </div>
+            <div>
+                <label for="address">recipient postcode:</label>
+                <input type="number" name="recipient_postcode" id="address" autocomplete="postal-code" required>
+            </div>
+            <div>
+                <label for="address">recipient phone:</label>
+                <input type="number" name="recipient_phone" id="address" required>
+            </div>
+            <div>
+                <label for="weight">Package weight:</label>
+                <input type="number" name="weight" id="weight" required>
+            </div>
 
-      <input type="submit" value="Send">
+            <input type="submit" value="Send">
 
-    </form>
+        </form>
 
-    {{-- style sheet for table --}}
-    <style>
-      table {
-        border-collapse: separate;
-        border-spacing: 20px 0;
-      }
+        {{-- style sheet for table --}}
+        <style>
+            table {
+                border-collapse: separate;
+                border-spacing: 20px 0;
+            }
 
-      th {
-        background-color: #4287f5;
-        color: white;
-      }
-      th,
-      td {
-        width: 150px;
-        text-align: center;
-        border: 1px solid black;
-        padding: 5px;
-      }
-      h2 {
-        color: #4287f5;
-      }
-    </style>
+            th {
+                background-color: #4287f5;
+                color: white;
+            }
 
-    <div class="parcel-list center">
-      <h2>List of parcel status</h2>
-      <table>
-        <tr>
-          <th>Sender ID</th>
-          <th>Name</th>
-          <th>Gender</th>
-          <th>Age</th>
-        </tr>
-        <tr>
-          <td class="td">10001</td>
-          <td>Tom</td>
-          <td>M</td>
-          <td>30</td>
-        </tr>
-        <tr>
-          <td class="td">10002</td>
-          <td>Sally</td>
-          <td>F</td>
-          <td>28</td>
-        </tr>
-        <tr>
-          <td class="td">10003</td>
-          <td>Emma</td>
-          <td>F</td>
-          <td>24</td>
-        </tr>
-      </table>
-    </div>
-  </main>
+            th,
+            td {
+                width: 150px;
+                text-align: center;
+                border: 1px solid black;
+                padding: 5px;
+            }
+
+            h2 {
+                color: #4287f5;
+            }
+
+        </style>
+
+        <div class="parcel-list center">
+            <h2>List of parcel status</h2>
+            <table>
+                <tr>
+                    <th>Sender ID</th>
+                    <th>Name</th>
+                    <th>Gender</th>
+                    <th>Age</th>
+                </tr>
+                <tr>
+                    <td class="td">10001</td>
+                    <td>Tom</td>
+                    <td>M</td>
+                    <td>30</td>
+                </tr>
+                <tr>
+                    <td class="td">10002</td>
+                    <td>Sally</td>
+                    <td>F</td>
+                    <td>28</td>
+                </tr>
+                <tr>
+                    <td class="td">10003</td>
+                    <td>Emma</td>
+                    <td>F</td>
+                    <td>24</td>
+                </tr>
+            </table>
+        </div>
+    </main>
 </body>
 
 </html>
