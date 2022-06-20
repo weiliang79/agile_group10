955325c540d0d4e0a62e3bfef76b8912cfc22277
ling xiao <lingxiao3218@gmail.com>
2022-06-20 14:27:14 +0800

    new manager test

---
+++ tests/Feature/ManagerControllerTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class ManagerControllerTest extends TestCase
+{
+    use RefreshDatabase, WithFaker;
+    
+    /**
+     * ling xiao
+     */
+    public function test_index()
+    {
+    }
+    
+    /**
+     * ling xiao
+     */
+    public function test_tracking_in_transit()
+    {
+    }
+    
+    /**
+     * ling xiao
+     */
+    public function test_tracking_in_transit_single()
+    {
+    }
+    
+    /**
+     * ling xiao
+     */
+    public function test_tracking_not_dispatch()
+    {
+    }
+    
+}
