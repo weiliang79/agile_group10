a1c53308b3532c03ca50aa965c1f8c82e96d843d
ling xiao <lingxiao3218@gmail.com>
2022-06-24 13:29:52 +0800

    test: create test for parcel request

--- tests/Unit/CourierControllerTest.php
+++ tests/Unit/CourierControllerTest.php
@@ -58,4 +58,14 @@ class CourierControllerTest extends TestCase
     {
         
     }
+
+    /**
+     * lingxiao
+     */
+    public function test_parcel_request_list()
+    {
+        $users = User::courier()->first();
+        $response = $this->actingAs($users)->post(route('courier.parcel_request_list'));
+        $response->assertStatus(200);
+    }
 }
