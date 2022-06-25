b00698f1fecc1967f80930c212a4bd05a7fb249a
ling xiao <lingxiao3218@gmail.com>
2022-06-25 15:13:15 +0800

    feat: 2 acceptance test gherkins

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -1,15 +1,22 @@
 <?php
 
+use App\Models\Role;
+use App\Models\User;
 use Behat\Behat\Tester\Exception\PendingException;
 use Behat\Behat\Context\Context;
 use Behat\Gherkin\Node\PyStringNode;
 use Behat\Gherkin\Node\TableNode;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\CreatesApplication;
+use Tests\TestCase;
 
 /**
  * Defines application features from the specific context.
  */
-class FeatureContext implements Context
+class FeatureContext extends TestCase implements Context
 {
+    use CreatesApplication, WithFaker;
+
     /**
      * Initializes context.
      *
@@ -19,30 +26,49 @@ class FeatureContext implements Context
      */
     public function __construct()
     {
+        putenv('APP_ENV=testing');
+        parent::__construct();
+        $this->setUp();
+        $this->withExceptionHandling();
+        $this->parcel = $this->newParcelPayload();
     }
 
-    /**
-     * @Given there is a courier (n) :arg1
-     */
-    public function thereIsAWhichCostsPs($arg1)
-    {
-        
-    }
 
     /**
      * @Given I am a normaluser
      */
     public function iAmANormaluser()
     {
-        throw new PendingException();
+        $this->actingUser = User::normaluser()->first();
+    }
+
+    /** @BeforeScenario */
+    public function before()
+    {
+        $this->artisan('migrate:fresh --seed');
     }
 
     /**
-     * @Given there is a courier with name :arg1
+     * @Given there is a courier with name :couriername
      */
-    public function thereIsACourierWithName($arg1)
+    public function thereIsACourierWithName($couriername)
     {
-        throw new PendingException();
+        //courier3
+        $this->actingCourier = User::create([
+            'username' => $couriername,
+            'first_name' => $couriername,
+            'last_name' => '',
+            'email' => $couriername . '@isp.com',
+            'password' => bcrypt('courier123'),
+            'gender' => '1',
+            'phone' => '0123456789',
+        ])
+            ->role()->associate(Role::ROLE_COURIER)
+            ->save();
+
+        $this->assertDatabaseHas('users', [
+            'username' => $couriername
+        ]);
     }
 
     /**
@@ -50,14 +76,47 @@ class FeatureContext implements Context
      */
     public function iBrowseToTheHomepage()
     {
-        throw new PendingException();
+        $this->response = $this->actingAs($this->actingUser)
+            ->get(route('normal_user.home'));
     }
 
     /**
-     * @Then I should see a courier named :arg1
+     * @Then I should see a courier named :couriername
      */
-    public function iShouldSeeACourierNamed($arg1)
+    public function iShouldSeeACourierNamed($couriername)
     {
-        throw new PendingException();
+        $this->response->assertSee($couriername);
+    }
+
+    /**
+     * @Given I submit the form with all the detail
+     */
+    public function iSubmitTheFormWithAllTheDetail()
+    {
+        $this->response = $this->actingAs($this->actingUser)
+            ->post(route('normal_user.save_parcel'), $this->parcel);
+    }
+
+    /**
+     * @Then I should see a parcel with detail I wrote
+     */
+    public function iShouldSeeAParcelWithDetailIWrote()
+    {
+        $this->assertDatabaseHas('parcels', $this->parcel);
+    }
+
+    private function newParcelPayload()
+    {
+        return [
+            'sender_address' => $this->faker->address(),
+            "sender_postcode" => $this->faker->postcode(),
+            "recipient_firstname" => $this->faker->firstName(),
+            "recipient_lastname" => $this->faker->lastName(),
+            "recipient_address" => $this->faker->address(),
+            "recipient_postcode" => $this->faker->postcode(),
+            "recipient_phone" => $this->faker->phoneNumber(),
+            "weight" => $this->faker->randomNumber(2),
+            "courier_id" => User::courier()->first()->id,
+        ];
     }
 }
--- features/sender.feature
+++ features/sender.feature
@@ -1,13 +1,20 @@
 Feature: Send parcel
-  In order to buy products
-  As a customer
-  I need to be able to put interesting products into a basket
+  In order to send parcel
+  As a sender
+  I need to be able to submit a form to send parcel
 
   Rules:
   - At least one courier is avalible
 
-  Scenario: send a parcel
+  Scenario: must have courier to choose
     Given I am a normaluser
     And there is a courier with name "fedex"
     When I browse to the homepage
     Then I should see a courier named "fedex"
+
+  Scenario: able to send a parcel
+    Given I am a normaluser
+    And I submit the form with all the detail
+    When I browse to the homepage
+    Then I should see a parcel with detail I wrote
+
