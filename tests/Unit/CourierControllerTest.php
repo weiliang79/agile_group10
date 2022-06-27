931f07cc41d9b9153d089e6810314978cf349b82
weiliang79 <ling2010@hotmail.my>
2022-06-27 15:02:54 +0800

    test: unit test for login, courier, landing

--- tests/Unit/CourierControllerTest.php
+++ tests/Unit/CourierControllerTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Unit;
 
+use App\Models\Parcel;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
@@ -24,7 +25,9 @@ class CourierControllerTest extends TestCase
      */
     public function test_index()
     {
-        
+        $user = User::courier()->first();
+        $response = $this->actingAs($user)->get(route('courier.home'));
+        $response->assertStatus(200);
     }
 
     /**
@@ -32,7 +35,11 @@ class CourierControllerTest extends TestCase
      */
     public function test_update_parcel()
     {
-        
+        $user = User::courier()->first();
+        $this->actingAs($user)->get(route('courier.home'));
+        $response = $this->actingAs($user)->post(route('courier.update_parcel'), $this->updateParcelPayload());
+        $response->assertStatus(302);
+        $response->assertRedirect(route('courier.home'));
     }
 
     /**
@@ -67,5 +74,11 @@ class CourierControllerTest extends TestCase
         $users = User::courier()->first();
         $response = $this->actingAs($users)->get(route('courier.parcel_request_list'));
         $response->assertStatus(200);
+    }
+
+    private function updateParcelPayload(){
+        return [
+            "tracking_number" => Parcel::where('status', Parcel::STATUS_NOT_PICK_UP)->first(),
+        ];
     }
 }
--- tests/Unit/LandingControllerTest.php
+++ tests/Unit/LandingControllerTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Unit;
 
+use App\Models\Parcel;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
@@ -24,7 +25,8 @@ class LandingControllerTest extends TestCase
      */
     public function test_index()
     {
-        
+        $response = $this->get(route('landing'));
+        $response->assertStatus(200);
     }
 
     /**
@@ -32,6 +34,8 @@ class LandingControllerTest extends TestCase
      */
     public function test_tracking()
     {
-        
+        $tracking_number = Parcel::first()->tracking_number;
+        $response = $this->get(route('landing.tracking', ['tracking_number' => $tracking_number]));
+        $response->assertStatus(200);
     }
 }
