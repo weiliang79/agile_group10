d93787d5ba37d437d3aa6f6ba4a9715326b31b60
ling xiao <lingxiao3218@gmail.com>
2022-07-16 17:09:10 +0800

    test: fix ManagerTrackingSingleCourierTest

--- tests/Feature/ManagerTrackingSingleCourierTest.php
+++ tests/Feature/ManagerTrackingSingleCourierTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Models\Parcel;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
@@ -26,9 +27,23 @@ class ManagerTrackingSingleCourierTest extends TestCase
         // get the first manager in database
         $user = User::manager()->first();
         $response = $this->actingAs($user)->get(route("manager.tracking_in_transit_single", [
-            'courier_id' => ManagerTrackingSingleCourierTest::COURIER_USER_ID
+            'courier_id' => $this::COURIER_USER_ID
         ]));
 
         $response->assertStatus(200);
+    }
+
+    public function test_page_can_display_new_parcel_in_transit()
+    {
+        Parcel::factory()->create([
+            'status' => Parcel::STATUS_IN_TRANSIT,
+            'courier_id' => User::courier()->first(),
+            'recipient_firstname' => "test name"
+        ]);
+        $user = User::manager()->first();
+        $response = $this->actingAs($user)->get(route("manager.tracking_in_transit_single", [
+            'courier_id' => $this::COURIER_USER_ID
+        ]));
+        $response->assertSeeText('test name');
     }
 }
